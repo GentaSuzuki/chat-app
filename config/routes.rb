00c96bf4262f0ee581f18e7b16965c10e2a5ff1b
GentaSuzuki <71617008+GentaSuzuki@users.noreply.github.com>
2020-09-25 20:01:55 +0900

    チャットルームの削除機能の実装

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   root to: "rooms#index"#ルートパスをrooms/index.html.erbのビューファイルに設定する
   resources :users, only: [:edit, :update, ]
-  resources :rooms, only: [:new, :create] do#roomsが親messagesが子になるようにdoチャットルームに属しているメッセージになる
+  resources :rooms, only: [:new, :create, :destroy] do#roomsが親messagesが子になるようにdoチャットルームに属しているメッセージになる
    resources :messages, only: [:index, :create]#メッセージに結びつくルームの情報を含んだパスを受け取れる
   end
 end
