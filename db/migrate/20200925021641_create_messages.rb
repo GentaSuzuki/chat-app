71f181bde04c61038da4af166f3d7d1b130afefc
GentaSuzuki <71617008+GentaSuzuki@users.noreply.github.com>
2020-09-25 11:49:38 +0900

    データベースのカラム名修正

--- db/migrate/20200925021641_create_messages.rb
+++ db/migrate/20200925021641_create_messages.rb
@@ -1,7 +1,7 @@
 class CreateMessages < ActiveRecord::Migration[6.0]
   def change
     create_table :messages do |t|
-      t.string :comment #roomsテーブルにstring(文字（短文）)でcommentというカラム名を追加する
+      t.string :content #roomsテーブルにstring(文字（短文）)でcommentというカラム名を追加する
       t.references :user,foreign_key: true#foreign_keyは関連する他のテーブルのレコードの主キー(user)を値としてもつ
       t.references :room,foreign_key: true#                                            (room)
       t.timestamps
