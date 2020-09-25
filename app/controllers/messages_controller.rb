f2636ede1f6d47aa50374d4b0f732a40e5c81555
GentaSuzuki <71617008+GentaSuzuki@users.noreply.github.com>
2020-09-25 17:18:16 +0900

    メッセージの表示

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -2,6 +2,7 @@ class MessagesController < ApplicationController
   def index#一覧表示する
     @message = Message.new#Message.newで生成したモデルのインスタンス情報代入
     @room = Room.find(params[:room_id])#params(リクエストに含まれる)に含まれるroom_idを代入ルーティングをネストしているため
+    @messages = @room.messages.includes(:user)#チャットルームに紐づいている全てのメッセージが入っている@room.messagesを@messagesに代入includes(N＋１問題で解消)でユーザー情報を一度のアクセスでまとめて処理することができる
   end
 
   def create#データ投稿を行う
@@ -10,6 +11,7 @@ class MessagesController < ApplicationController
       if  @message.save#データを保存するmessagesテーブルに
         redirect_to room_messages_path(@room)#true 参加しているチャットルームに投稿したメッセージの一覧画面へ
       else
+        @messages = @room.messages.includes(:user)#投稿に失敗した内容も保持しつつ
         render :index# false indexが実行同じページへ戻る
       end
   end
