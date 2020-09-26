90d4bc185af5add6250153c75e541a1e6e693aa4
GentaSuzuki <71617008+GentaSuzuki@users.noreply.github.com>
2020-09-26 22:50:39 +0900

    各モデルテストのテストコード修正（メッセージ）

--- spec/models/message_spec.rb
+++ spec/models/message_spec.rb
@@ -14,14 +14,14 @@ RSpec.describe Message, type: :model do
       expect(@message).to be_valid
     end
     it 'imageが空でも保存できること' do
-      @message.image =nil
+      @message.image = nil
       expect(@message).to be_valid
     end
     it 'contentとimageが空では保存できないこと' do
       @message.content = nil
       @message.image = nil
       @message.valid?
-      expect(@message.errors.full_messages).to include("content can't be blank")
+      expect(@message.errors.full_messages).to include("Content can't be blank")
     end
     it 'roomが紐付いていないと保存できないこと' do
       @message.room = nil
